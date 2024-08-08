8c9648486b4cb264f9972de65e3c7be4afd174db
Srivastava <s.ck.srivastava@accenture.com>
2024-08-08 11:23:00 +0530

    cookies implementation

--- login.py
+++ login.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from app import add_footer
+from streamlit_cookies_manager import EncryptedCookieManager
 
-
+cookies = EncryptedCookieManager(
+    prefix="da_app",  # You can change the prefix to any string
+    password="da_pass"  # Replace with your own password
+)
 def login():
     logo_url = "static/ACN.svg"  # Replace with the URL or path to your logo
     st.image(logo_url, width=50, use_column_width=False)
@@ -14,6 +18,8 @@ def login():
     if st.button("Login"):
         if username == "admin" and password == "password":
             st.session_state['logged_in'] = True
+            cookies['logged_in'] = 'true'
+            cookies.save()
             st.experimental_rerun()
         else:
             st.error("Invalid username or password")
