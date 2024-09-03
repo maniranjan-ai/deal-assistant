63dc07d667085111c98a0c54ed2a882d365000c0
maniranjan.pandey <maniranjan.pandey@accenture.com>
2024-09-03 11:10:43 +0530

    streamlit secrets added

--- utils/key_data.py
+++ utils/key_data.py
@@ -2,10 +2,11 @@
 #
 # the_key=os.getenv("OPENAI_API_KEY")
 from dotenv import load_dotenv
+import streamlit as st
 import os
 
 # Load environment variables from .env file
 load_dotenv()
 
-the_key = os.getenv('OPENAI_API_KEY')
-da_pass = 'password'
+the_key = st.secrets["OPENAI_API_KEY"]
+da_pass = st.secrets["da_pass"]
